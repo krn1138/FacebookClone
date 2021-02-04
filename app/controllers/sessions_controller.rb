033ff27b1b29aab4a58d8886b6363e0f8fef1e80
oomiya karen <krn971138@gmail.com>
2021-02-04 12:11:26 +0900

    ログイン中のユーザーを取得する機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ログイン成功した場合
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
-      flash.now[:danger] = 'ログインに失敗しました'
+      flash[:danger] = 'ログインに失敗しました'
       render :new
     end
   end
